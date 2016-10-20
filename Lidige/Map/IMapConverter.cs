e0c63955dd9bf6ad8443ff3ce109e5104ff09ee7
Fpa <Fpa>
2016-10-20 14:50:13 +0200

    Map setup (WIP), not correct

---
+++ Lidige/Map/IMapConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lidige.Map
+{
+    public interface IMapConverter
+    {
+        IMap Convert(string map);
+    }
+}
---
+++ Lidige/Map/Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lidige.Map
+{
+    public class Map
+    {
+        private int _width;
+        private int _height;
+
+        public Map(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+    }
+}
