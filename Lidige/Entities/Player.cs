3d6b0e6b7dcd8078d1486f8844ff152869e9bd7c
Fabian Pas <fabianp@fusionzone.nl>
2016-11-03 22:08:51 +0100

    sprite movement

--- Lidige/Entities/Player.cs
+++ Lidige/Entities/Player.cs
@@ -9,19 +9,22 @@ using System.Threading.Tasks;
 namespace Lidige.Entities
 {
     public enum Direction {
-        Up,
-        Down,
-        Left,
-        Right
+        Up = 0,
+        Down = 3,
+        Left = 6,
+        Right = 9
     }
 
     public class Player
     {
         private readonly Texture2D _texture;
+
         private Vector2 _position = new Vector2(0, 0);
         private Vector2 _targetPosition;
+        private bool _isMoving;
+        private Direction _direction;
 
-        private const float Duration = 3f;
+        private const float Duration = 0.10f;
 
         public Player(Texture2D texture)
         {
@@ -30,13 +33,24 @@ namespace Lidige.Entities
 
         public void Update(float delta)
         {
-            var t = Duration * delta;
+            if (Vector2.Distance(_position, _targetPosition) < 0.1f)
+            {
+                _position = _targetPosition;
+                _isMoving = false;
+            }
+
+            var t = delta / Duration;
             _position = _position + (_targetPosition - _position) * t;
         }
 
         public void Move(Direction direction)
         {
-            switch(direction)
+            if (_isMoving)
+                return;
+
+            _isMoving = true;
+
+            switch (direction)
             {
                 case Direction.Down:
                     _targetPosition = _position + new Vector2(0, 1);
@@ -54,11 +68,14 @@ namespace Lidige.Entities
                     _targetPosition = _position - new Vector2(1, 0);
                     break;
             }
+
+            _direction = direction;
+            
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_texture, new Rectangle((int)_position.X * 32, (int)_position.Y * 32, 48, 64), new Rectangle(0, 0, 48, 64), Color.White);
+            spriteBatch.Draw(_texture, new Rectangle((int)_position.X * 32, (int)_position.Y * 32, 48, 64), new Rectangle((int)_direction * 48, 0, 48, 64), Color.White);
         }
 
         public Vector2 Position
